9ecd9a5e38364e656856ab2d79850a670814c2fe
nahuelgulias <145394037+n4huel77@users.noreply.github.com>
2023-11-11 22:31:42 -0300

    Cambios en el router.

--- router.php
+++ router.php
@@ -12,7 +12,7 @@ require_once 'libs/route.php';
     $router->addRoute('vinilos/:ID/:subrecurso',    'GET',    'ViniloController', 'getVinilo'); //Obtiene un campo especifico del vinilo .
     $router->addRoute('vinilos',                    'POST',   'ViniloController', 'insertarVinilo'); //Insertar vinilo.
     $router->addRoute('vinilos/:ID',                'PUT',    'ViniloController', 'modificarVinilo'); //Modifica vinilo.
-    $router->addRoute('items',                      'GET',    'ViniloController', 'getEntidades'); //??
+
     #Artistas          endpoint                     verbo      controller          metodo
     $router->addRoute('artistas',                   'GET',    'ArtistaController', 'getAllArtistas'); //Obtiene todos los artistas en caso de no indicar orden y columna.
     $router->addRoute('artistas/:ID',               'GET',    'ArtistaController', 'getArtista'); //Obtiene un solo artista.
